1b6be863f7b06a1413b3cfb12f646c7ddf178c2c
Zach-Turner3791 <zachary.turner9012@gmail.com>
2022-11-29 06:36:13 -0700

    updating portfolio container

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      data: [
+        {title: "Quip"},
+        {title: "Eventbrite"},
+        {title: "Ministry Safe"},
+        {title: "SwingAway"}
+      ]
+    }
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} />;
+    })
+  }
+
+  handlePageTitleUpdate() {
+    this.setState({
+      pageTitle: "Something Else"
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        {this.portfolioItems()}
+
+        <hr/>
+
+        <button onClick={this.handlePageTitleUpdate}>Change Title</button>
+      </div>
+    )
+  }
+}
