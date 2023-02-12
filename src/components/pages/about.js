abf92dc44239ebb7a8c7c555ba6450757c2ea471
Zach-Turner3791 <zachary.turner9012@gmail.com>
2023-02-11 20:23:32 -0700

    added content to the about me page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function() {
   return (
     <div>
-        About
+        I have honed my software development skills through "Bottega University". My background is largely in operations, but I am currently in school to further my education in full-stack development. I have a strong passion for coding and have acquired skills in HTML, CSS, SCSS, JavaScript, Python, MySQL, and Git. I am eager to use my skills to contribute to the tech industry and make a meaningful impact. My ultimate goal is to become a reliable and efficient software engineer who can deliver high-quality products. When I'm not coding, I love immersing myself in the world of fantasy, particularly the works of J.R.R. Tolkien.
     </div>
   );
 }
